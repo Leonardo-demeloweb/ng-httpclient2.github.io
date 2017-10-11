ac08c96dcbe29f055fce051e20a0df08567d9bfa
YOUR NAME <demelo01@gmailcom>
2017-10-11 11:09:41 -0300

    Save my work

---
+++ src/app/githubauth.interceptor.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs/observable';
+
+@Injectable()
+export class GithubAuthInterceptor implements HttpInterceptor {
+  intercept (req: HttpRequest<any>, next:HttpHandler): Observable<HttpEvent<any>> {
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', 'token dbd0ad422d720d1d7a5af33509860f2b1900b84e')
+    })
+    return next.handle(authReq);
+  }
+
+}
